3e5e7d0be84fff32183bb1742cf05c02d0b6bd69
hladrien <hladrien@d6b7afda-8ff4-ed69-7695-3614b13b2274>
2011-05-09 21:10:19 +0000

    ajout des tests papa et hladrien + emplacement de la lib compile

---
+++ trunk/trunk/tests/papa/webCap/compil.sh
@@ -0,0 +1,1 @@
+cc -W -Wall main.c filters.c -l cv -l highgui -l cxcore -o webcap
---
+++ trunk/trunk/tests/papa/webCap/filters.h
@@ -0,0 +1,19 @@
+#ifndef FILTERS_H_
+# define FILTERS_H_
+
+# include <opencv/cv.h>
+# include <opencv/highgui.h>
+
+typedef char bool;
+# define false 0
+# define true  1
+
+# define FT_NONE      0
+# define FT_NEGATIVE  1
+# define FT_GRAYSCALE 2
+
+void apply_filter(IplImage* frame, int filter, bool apply);
+void filter_negative(IplImage* frame);
+void filter_grayscale(IplImage* frame);
+
+#endif
