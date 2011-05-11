0e8babf177a52be2c1d89cfedfbc0f9737a6ce8d
f.lalande <f.lalande@d6b7afda-8ff4-ed69-7695-3614b13b2274>
2011-05-11 08:48:38 +0000

    Ajout d'un debut de YUV + downsampling dans le dossier tests, mais il y a encore un probleme d'arrondi

--- trunk/trunk/tests/papa/webCap/compil.sh
+++ trunk/trunk/tests/papa/webCap/compil.sh
@@ -1,1 +1,1 @@
-cc -W -Wall main.c filters.c -l cv -l highgui -l cxcore -o webcap
+cc -W -Wall main.c filters.c downsampling.c -l cv -l highgui -l cxcore -l m -o webcap
--- trunk/trunk/tests/papa/webCap/filters.h
+++ trunk/trunk/tests/papa/webCap/filters.h
@@ -8,12 +8,14 @@ typedef char bool;
 # define false 0
 # define true  1
 
-# define FT_NONE      0
-# define FT_NEGATIVE  1
-# define FT_GRAYSCALE 2
+# define FT_NONE         0
+# define FT_NEGATIVE     1
+# define FT_GRAYSCALE    2
+# define FT_DOWNSAMPLING 3
 
-void apply_filter(IplImage* frame, int filter, bool apply);
-void filter_negative(IplImage* frame);
-void filter_grayscale(IplImage* frame);
+void apply_filter(IplImage *frame, int filter, bool apply);
+void filter_negative(IplImage *frame);
+void filter_grayscale(IplImage *frame);
+void filter_downsampling(IplImage *frame);
 
 #endif
