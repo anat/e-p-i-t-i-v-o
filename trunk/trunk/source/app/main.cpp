9eeed1c0e03f71df1c5baf746fb0bcdc4a0004a5
hladrien <hladrien@d6b7afda-8ff4-ed69-7695-3614b13b2274>
2011-07-01 10:19:56 +0000

    envoi elec cam

--- trunk/trunk/source/app/main.cpp
+++ trunk/trunk/source/app/main.cpp
@@ -3,9 +3,36 @@
 #include "GUILoader.hpp"
 #include "ConfigManager.hpp"
 
+//test cam
+#include  <set>
+#include  <iostream>
+#include <cv.h>
+#include <cv.hpp>
+#include <cvaux.h>
+#include <highgui.h>
+
 using namespace vm;
 
 const std::string EpitivoConfFile = "epitivo.conf.xml";
+
+std::set<int> getCamDevices() 
+{
+  std::set<int> setDispos;
+  CvCapture* capTmp;
+  int camCount = 0;
+  for (int i = 0; camCount < sizeof (1000); i++) {
+    capTmp = cvCreateCameraCapture(i);
+    if (capTmp) {
+      setDispos.insert(i);
+      cvReleaseCapture(&capTmp);
+      std::cout << i << " dispo" << std::endl;
+      camCount++;
+    } else {
+      //std::cout << i << " pas disso" << std::endl;
+    }
+  }
+  return setDispos;
+}
 
 int main(int argc, char** argv)
 {
@@ -16,5 +43,6 @@ int main(int argc, char** argv)
   gui.Show();
 
   return gui.exec();
+  getCamDevices();
 }
 
